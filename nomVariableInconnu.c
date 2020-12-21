50fab7279819020037f73089f0000892fb543852
JaryVenAutocar <59994606+JaryVenAutocar@users.noreply.github.com>
2020-12-22 00:48:08 +0100

    Add files via upload

--- nomVariableInconnu.c
+++ nomVariableInconnu.c
@@ -3,9 +3,7 @@
 
   int fact(int n, bool b, bool c) {
     if (n < 2) {
-	for(int i = 0; i < n; i++){
-      		PARAM = PARAM + 2;
-	}
+      		PARA = PARA + 2;
     } else {
       return n * fact(n + 1, -1, true);
     }
